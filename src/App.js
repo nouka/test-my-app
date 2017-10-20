fba8c4339bae8f85b003e67532e4d2a913b917b2
ノーカ <t-nouka@oz-vision.co.jp>
2017-10-21 00:10:40 +0900

    reactをgithub-pageにデプロイ

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Welcome extends Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>
-  }
+function Header() {
+  return (
+    <h1><img src={logo} alt="logo"/></h1>
+  );
+}
+
+function Footer() {
+  return (
+    <p className="copyright">Copyright &copy; Hoge</p>
+  );
 }
 
 class App extends Component {
   render() {
-    return <Welcome name="asdfasdfsadfasdf" />;
+    return (
+      <div>
+        <Header></Header>
+        <Footer></Footer>
+      </div>
+    );
   }
 }
 
